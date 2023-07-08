d67fcf7c476527a0deec1eb652fcebb5d9945d2c
isdiscodead <isdiscodead@korea.ac.kr>
2023-07-08 20:55:30 +0900

    -

--- first-ionic-react/myApp/src/contexts/ThemeContext.ts
+++ first-ionic-react/myApp/src/contexts/ThemeContext.ts
@@ -7,11 +7,10 @@ type Theme = {
 
 // as 연산자로 initial value 없이 사용할 수 있음 
 export const ThemeContext = createContext({} as Theme); // 인자 = 초기값
-ThemeContext.Provider
+
 
 // API 호출 등 data에 대해서 복잡한 로직이 필요할 경우 Provider도 작성해서 export하여 사용하는 것이 좋음 ?? 
 /* 
-
 export const ThemeProvider = (children: React.ReactNode ) => {
     // logics & states ... 
     
@@ -19,5 +18,4 @@ export const ThemeProvider = (children: React.ReactNode ) => {
         <ThemeContext.Provider value={...}>{ children }</ThemeContext.Provider>
     )
 }
-
 */
